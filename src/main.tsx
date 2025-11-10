124d9cb78a9dd98115b63309c9cf92a55b17a511
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-11-10 21:04:59 +0100

    run

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './hooks/AuthContext';
+import './api/testApi'; // Import testing utilities
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+);
